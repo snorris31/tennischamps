855ab04446a6b0a0b9e82206db2e4d0b9e6a8dc0
lixinwang <lixin@lawn-128-61-77-16.lawn.gatech.edu>
2018-02-02 10:39:57 -0500

    Fix back button

--- Home/homepage.js
+++ Home/homepage.js
@@ -21,7 +21,19 @@ export default class WelcomeScreen extends React.Component {
     this.setState({ fontLoaded: true });
   }
 
+  async componentDidMount() {
+    await Expo.Font.loadAsync({
+      'bungee-inline': require('../assets/fonts/BungeeInline-Regular.ttf'),
+      'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+      'Roboto_medium': require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    const { navigation } = this.props;
+    if (!this.state.fontLoaded) { return null;}
+    
     return (
       <Container>
         <Navbar
