9bcfa845a87ec3447eaae4e7cd6545a51b5bc05e
lixinwang <lixin@lawn-128-61-77-16.lawn.gatech.edu>
2018-02-02 10:28:48 -0500

    Add navbar

--- Home/homepage.js
+++ Home/homepage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Container, Content } from 'native-base';
 import { Font } from 'expo';
 
 import Button from '../Components/Button';
+import Navbar from '../Components/Navbar';
 
 export default class WelcomeScreen extends React.Component {
   constructor(props) {
@@ -21,65 +23,52 @@ export default class WelcomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <Container>
+        <Navbar
+          title='HOME'
+          onPressBack={() => navigation.goBack(null)}/>
 
+        <Content contentContainerStyle={styles.content}>
+          <Button style={styles.button}
+           label='PLAY'
+           onPress={() => this.props.navigation.navigate("LogIn")}
+          />
 
-        <Button style={styles.button}
-         label='PLAY'
-         onPress={() => this.props.navigation.navigate("LogIn")}
-        />
+         <Button style={styles.button}
+          label='HOW TO PLAY'
+          onPress={() => this.props.navigation.navigate("Registration")}
+          />
 
-       <Button style={styles.button}
-        label='HOW TO PLAY'
-        onPress={() => this.props.navigation.navigate("Registration")}
-        />
+          <Button style={styles.button}
+          label='SET PREFERENCE'
+          onPress={() => this.props.navigation.navigate("Registration")}
+          />
 
-        <Button style={styles.button}
-        label='SET PREFERENCE'
-        onPress={() => this.props.navigation.navigate("Registration")}
-        />
+          <Button style={styles.button}
+          label='MY STATS'
+          onPress={() => this.props.navigation.navigate("Registration")}
+          />
 
-        <Button style={styles.button}
-        label='MY STATS'
-        onPress={() => this.props.navigation.navigate("Registration")}
-        />
-
-        <Button style={styles.button}
-        label='LOG OUT'
-        onPress={() => this.props.navigation.navigate("Registration")}
-        />
-
-      </View>
+          <Button style={styles.button}
+          label='LOG OUT'
+          onPress={() => this.props.navigation.navigate("Home")}
+          />
+        </Content>
+      </Container>
     );
   }
 }
 
 
 const styles = StyleSheet.create({
-  container: {
+  content: {
     flex: 1,
     backgroundColor: '#2A5D38',
     alignItems: 'center',
     justifyContent: 'center'
   },
   button: {
-    alignItems: 'center',
     backgroundColor: '#ffffff',
     margin: 18,
-    width: 250,
-    paddingVertical: 10,
-    borderRadius: 5
-  },
-  image: {
-    width: 180,
-    height: 180,
-    margin: 40
-  },
-  titleText: {
-    fontFamily: 'bungee-inline',
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    marginBottom: 40
   }
 });
