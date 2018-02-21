480d47ab827b0191fe3fd7e90673e6f704acc1ef
Matthew Kennedy <mk@gatech.edu>
2018-02-21 11:27:21 -0500

    Add game backend file with stubs

---
+++ Model/TennisGame.js
@@ -0,0 +1,21 @@
+
+
+export default class TennisGame {
+    getRandomShotTarget() {
+        // ieee standard dice roll
+        // xkcd.com/221
+        return 4;
+    }
+
+    getRandomShotType() {
+        // ieee standard dice roll
+        // xkcd.com/221
+        return 4;
+    }
+
+    getShotResult(targetLoc, playerPosition, shotDirection, shotPower) {
+        
+
+        
+    }
+}
