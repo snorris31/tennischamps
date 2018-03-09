10e7c7445b71c0d432dcf6356b28966c7c3e0eb9
Matthew Kennedy <mk@gatech.edu>
2018-03-09 05:18:20 -0500

    Fix home hamburger

--- HomeScreen/Home.js
+++ HomeScreen/Home.js
@@ -43,8 +43,8 @@ export default class Home extends React.Component {
       <Container>
         <Navbar
           title='HOME'
-          onPressBack={() => navigation.goBack(null)}
-          handleHamburger={() => navigation.navigate('DrawerOpen')}/>
+          onPressBack={() => this.props.navigation.goBack(null)}
+          handleHamburger={() => this.props.navigation.navigate('DrawerOpen')}/>
 
         <Content contentContainerStyle={styles.content}>
           <Button style={styles.button}
