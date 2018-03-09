6ae3e0897f3b396bd42a77bd9ae2fd609af35ecf
Matthew Kennedy <mk@gatech.edu>
2018-03-09 02:48:09 -0500

    move getAccuracy to game model class

--- Model/TennisGame.js
+++ Model/TennisGame.js
@@ -18,4 +18,24 @@ export default class TennisGame {
 
         
     }
+
+    getAccuracy(shotCoordinate, shotTarget) {
+        let [x1, y1] = shotCoordinate
+        let [x2, y2] = shotTarget
+        // pythagorean theorem
+        // sqrt(dx^2 + dy^2)
+        distance = (((x1 - x2)**2) + ((y1 - y2)**2))**0.5
+        if (distance < 5) {
+          return "veryclose"
+        }
+        if (distance > 5 && distance <= 15) {
+          return "close"
+        }
+        if (distance > 15 && distance <= 25) {
+          return "average"
+        }
+        if (distance > 25) {
+          return "far"
+        }
+      }
 }
